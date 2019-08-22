091e8d6cbb6318ff3671160d771069eb757db7b9
EJ <ejg132@gmail.com>
2019-08-22 12:09:07 -0700

    finished dogs

--- index.js
+++ index.js
@@ -8,34 +8,59 @@ function getDogImage() {
     .then(responseJson => displayImage(responseJson));
 }
 
-function displayImage(responseJSON) {
-  $('.dogImage').append(`
-  <img src='${responseJSON.message}' id='dogImage'>
-  `);
-}
+function displayImage(responseJSONspecific) {
 
-function howMany() {
-  const dogValue = $('.js-dogAmount').val();
-  console.log(dogValue);
+  console.log(responseJSONspecific);
 
-  if (dogValue > 50){
-    $('ul').html(`Sorry you can only reach a limit of 50 dog images!`)
-  } else {
-    for (let i = 0; i < dogValue; i++){
-      getDogImage();
-      console.log('this loop is running')
-    }
+  if (responseJSONspecific.code === 404) {
+    alert('404 NOT FOUND')
+    return null;
   }
 
+  $('.dogImage').append(`
+    <img src='${responseJSONspecific.message}' id='dogImage'>
+    `);
+  console.log(responseJSONspecific.message);
+
 }
 
+function whatDog(breed) {
+
+  let dogType = breed;
+    fetch(`https://dog.ceo/api/breed/${dogType}/images/random`)
+      .then(response => response.json())
+      .then(responseJSONspecific => displayImage(responseJSONspecific))
+
+}
+
+// function howMany() {
+//   const dogValue = $('.js-dogAmount').val();
+//   console.log(dogValue);
+
+//   if (dogValue > 50){
+//     $('ul').html(`Sorry you can only reach a limit of 50 dog images!`)
+//   } else if (!dogValue){
+//     getDogImage()
+//   } else {
+//     for (let i = 0; i < dogValue; i++){
+//       getDogImage();
+//       whatDog('poodle');
+//       console.log('this loop is running')
+//     }
+//   }
+
+// }
+
 // extract the retrieved data
-function extractData(){
-  $('form').submit( function(event) {
+function extractData() {
+  $('form').submit(function (event) {
     console.log('submit button pressed');
     event.preventDefault();
     $('ul').empty();
-    howMany();
+    whatDog($('.js-dogType').val());
+    $('.js-dogType').val('');
+    // howMany();
+    // $('.js-dogAmount').val('');
   });
 }
 
@@ -45,7 +70,4 @@ function extractData(){
   return 
 } */
 
-$(getDogImage);
-$(getDogImage);
-$(getDogImage);
 $(extractData);
